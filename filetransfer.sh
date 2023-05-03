7227857afb82bb483f1fa01dfca50e759b550379
sasha_kr <a.krutseniuk@gmail.com>
2023-05-03 19:18:59 +0300

    fix

--- filetransfer.sh
+++ filetransfer.sh
@@ -14,7 +14,8 @@ scp $FILE_PATH $RECEIVER_USER@$RECEIVER_HOST:/home/ubuntu/received_file >> $LOG_
 # Получение размера файла в битах
 FILE_SIZE=$(stat -c%s "$FILE_PATH")
 
-ssh -T $RECEIVER_USER@$RECEIVER_HOST << EOF 2>&1 | grep -v "Welcome to Ubuntu" | tee -a $LOG_FILE
+# Отправка команды на сервер-получатель для проверки файла и ожидание подтверждения
+ssh -T $RECEIVER_USER@$RECEIVER_HOST << EOF | grep -E "File received successfully\. Size match\.|File size doesn't match\. Something went wrong\." >> $LOG_FILE
 RECEIVED_FILE_SIZE=\$(stat -c%s "/home/ubuntu/received_file")
 
 if [ \$RECEIVED_FILE_SIZE -eq $FILE_SIZE ]; then
