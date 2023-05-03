b48463890c4102ba93ed5169c356b144bd6e2bef
sasha_kr <a.krutseniuk@gmail.com>
2023-05-03 20:38:53 +0300

    fix

--- filetransfer.sh
+++ filetransfer.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 # файл для відправки
-FILE_PATH="/home/ubuntu/testfile6"
+file_path="/home/ubuntu/testfile7"
 # адреса отримувача
-RECEIVER_HOST=54.187.59.142
-RECEIVER_USER="ubuntu" # користувач на ремоут-хості
-LOG_FILE="/home/ubuntu/logfile.log" # файл з результатом перевірки розміру файлу (відправлений=отриманий)
-dest_file="/home/ubuntu/received_file6" # місце і назва переданого файлу на ремоут-хості(отримувачі)
-CURRENT_DATE=$(date +"%Y-%m-%d %H:%M:%S") # дата перевірки розмірів файлів
+receiver_host=54.187.59.142 
+receiver_user="ubuntu" # користувач на ремоут-хості 
+log_file="/home/ubuntu/logfile.log" # файл з результатом перевірки розміру файлу (відправлений=отриманий)
+dest_file="/home/ubuntu/received_file7" # місце і назва переданого файлу на ремоут-хості(отримувачі)
+current_date=$(date +"%Y-%m-%d %H:%M:%S") # дата перевірки розмірів файлів
 # Отправка файла на сервер-получатель
-scp $FILE_PATH $RECEIVER_USER@$RECEIVER_HOST:$dest_file >> $LOG_FILE #2>&1
+scp $file_path $receiver_user@$receiver_host:$dest_file >> $log_file  
 
 # Получение размера файла в битах
-FILE_SIZE=$(stat -c%s "$FILE_PATH")
+file_size=$(stat -c%s "$file_path")
 
 # Отправка команды на сервер-получатель для проверки файла и ожидание подтверждения
-ssh -T $RECEIVER_USER@$RECEIVER_HOST << EOF | grep -E "File received successfully\. Size match\.|File size doesn't match\. Something went wrong\." >> $LOG_FILE
-RECEIVED_FILE_SIZE=\$(stat -c%s "$dest_file")
+ssh -T $receiver_user@$receiver_host << EOF | grep -E "File received successfully\. Size match\.|File size doesn't match\. Something went wrong\." >> $LOG_FILE
+received_file_size=\$(stat -c%s "$dest_file")  
 
-if [ \$RECEIVED_FILE_SIZE -eq $FILE_SIZE ]; then
-    echo "$CURRENT_DATE File received successfully. Size match."
+if [ \$received_file_size -eq $file_size ]; then
+    echo "$current_date File received successfully. Size match."
 else
-    echo "$CURRENT_DATE File size doesn't match. Something went wrong."
+    echo "$current_date File size doesn't match. Something went wrong."
 fi
 EOF
