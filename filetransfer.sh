108a26829ab2d209407e218b6ee278947520f838
sasha_kr <a.krutseniuk@gmail.com>
2023-05-03 17:04:14 +0300

    test

--- filetransfer.sh
+++ filetransfer.sh
@@ -3,18 +3,19 @@
 # Имя файла и путь к нему
 FILE_PATH="/home/ubuntu/testfile"
 # IP или имя сервера-получателя
-RECEIVER_HOST=ubuntu@54.187.59.142
+RECEIVER_HOST=54.187.59.142
+RECEIVER_USER="ubuntu"
 # Путь к файлу лога
 LOG_FILE="/home/ubuntu/logfile.log"
 
 # Отправка файла на сервер-получатель
-scp $FILE_PATH $RECEIVER_HOST:~/received_file >> $LOG_FILE
+scp $FILE_PATH $RECEIVER_USER@$RECEIVER_HOST:~/received_file >> $LOG_FILE 2>&1
 
 # Получение размера файла в битах
 FILE_SIZE=$(stat -c%s "$FILE_PATH")
 
 # Отправка команды на сервер-получатель для проверки файла и ожидание подтверждения
-ssh $RECEIVER_HOST << EOF >> $LOG_FILE
+ssh -t -q $RECEIVER_USER@$RECEIVER_HOST << EOF >> $LOG_FILE
 RECEIVED_FILE_SIZE=\$(stat -c%s "~/received_file")
 
 if [ \$RECEIVED_FILE_SIZE -eq $FILE_SIZE ]; then
