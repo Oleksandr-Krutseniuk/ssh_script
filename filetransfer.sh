d464fde589633305c2fdc11a49fd9da21f5c74d3
sasha_kr <a.krutseniuk@gmail.com>
2023-05-03 20:13:14 +0300

    fix

--- filetransfer.sh
+++ filetransfer.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-# Имя файла и путь к нему
+# файл для відправки
 FILE_PATH="/home/ubuntu/testfile5"
-# IP или имя сервера-получателя
+# адреса отримувача
 RECEIVER_HOST=54.187.59.142
-RECEIVER_USER="ubuntu"
-# Путь к файлу лога
-LOG_FILE="/home/ubuntu/logfile.log"
-dest_file="/home/ubuntu/received_file5"
-CURRENT_DATE=$(date +"%Y-%m-%d %H:%M:%S")
+RECEIVER_USER="ubuntu" # користувач на ремоут-хості
+LOG_FILE="/home/ubuntu/logfile.log" # файл з результатом перевірки розміру файлу (відправлений=отриманий)
+dest_file="/home/ubuntu/received_file5" # місце і назва переданого файлу на ремоут-хості(отримувачі)
+CURRENT_DATE=$(date +"%Y-%m-%d %H:%M:%S") # дата перевірки розмірів файлів
 # Отправка файла на сервер-получатель
-scp $FILE_PATH $RECEIVER_USER@$RECEIVER_HOST:$dest_file >> $LOG_FILE 2>&1
+scp $FILE_PATH $RECEIVER_USER@$RECEIVER_HOST:$dest_file >> $LOG_FILE #2>&1
 
 # Получение размера файла в битах
 FILE_SIZE=$(stat -c%s "$FILE_PATH")
