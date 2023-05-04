8d27b41d95c693ad325714aa384d3be590478679
sasha_kr <a.krutseniuk@gmail.com>
2023-05-04 12:08:34 +0300

    hash_sums_check

--- filetransfer_bytes_check.sh
+++ filetransfer_bytes_check.sh
@@ -1,4 +1,4 @@
-#!/bin/bash          ЧЕРНОВИК(Попробовать запустить этот)
+#!/bin/bash          РАБОЧАЯ ВЕРСИЯ С ПРОВЕРКОЙ ПО БАЙТАМ
 
 # файл для відправки
 file_path="/home/ubuntu/testfile"
@@ -6,26 +6,24 @@ file_path="/home/ubuntu/testfile"
 receiver_host=34.220.97.124 
 receiver_user="ubuntu" # користувач на ремоут-хості 
 log_file="/home/ubuntu/logfile.log" # файл з результатом перевірки розміру файлу (відправлений=отриманий)
-#dest_file="/home/ubuntu/received_file" # місце і назва переданого файлу на ремоут-хості(отримувачі)
 current_date=$(date +"%Y-%m-%d %H:%M:%S" ) # дата перевірки розмірів файлів
 
   
-# архивирование файла
+# архівація файлу
 archive_name="$(basename ${file_path}).tar.gz" # тут буде назва файлу для архівації + ".tar.gz"
  
-tar -czf "$archive_name" -C "$(dirname ${file_path})" "$(basename ${file_path})"
+tar -czf "$archive_name" -C "$(dirname ${file_path})" "$(basename ${file_path})" # скинуть вывод, ато он светится в терминале
 #        | назва архіву    | місце файлу для архівації  | назва файлу для архівації
 
-
 # віправка файлу на сервер-отримувач 
-scp "$archive_name" "$receiver_user@$receiver_host:/home/ubuntu/$archive_name" >> $log_file
+scp "$archive_name" "$receiver_user@$receiver_host:/home/ubuntu/$archive_name" # якщо потрібно - можна створити змінну для місця зберігання
 
-# Получение размера файла в битах
+# розмір в бітах
 file_size=$(stat -c%s "$archive_name") #поменять на сравнение размера архивов
 
 # Отправка команды на сервер-получатель для проверки файла и ожидание подтверждения
 ssh -T $receiver_user@$receiver_host << EOF | grep -E "File received successfully\. Size match\.|File size doesn't match\. Something went wrong\." >> $log_file
- 
+# grep буде виводити тільки строки з значеннями з циклу if (цикл нижче) 
  
 received_file_size=\$(stat -c%s "/home/ubuntu/$archive_name")
 
@@ -37,8 +35,3 @@ received_file_size=\$(stat -c%s "/home/ubuntu/$archive_name")
     echo "$current_date File size doesn't match. Something went wrong."
   fi
 EOF
-
-# в конце можно добавить удаление бекапа если условие тру-можно добавить цикл для сервера-отправителя. например:
-#if [ $? -eq 0 ]; then
-#    rm -f $archive_name
-#fi
--- transfer_file_works.sh
+++ transfer_file_works.sh
@@ -6,39 +6,33 @@ file_path="/home/ubuntu/testfile"
 receiver_host=34.220.97.124 
 receiver_user="ubuntu" # користувач на ремоут-хості 
 log_file="/home/ubuntu/logfile.log" # файл з результатом перевірки розміру файлу (відправлений=отриманий)
-#dest_file="/home/ubuntu/received_file" # місце і назва переданого файлу на ремоут-хості(отримувачі)
 current_date=$(date +"%Y-%m-%d %H:%M:%S" ) # дата перевірки розмірів файлів
 
   
 # архівація файлу
 archive_name="$(basename ${file_path}).tar.gz" # тут буде назва файлу для архівації + ".tar.gz"
  
-tar -czf "$archive_name" -C "$(dirname ${file_path})" "$(basename ${file_path})"
+tar -czf "$archive_name" -C "$(dirname ${file_path})" "$(basename ${file_path})" # скинуть вывод, ато он светится в терминале
 #        | назва архіву    | місце файлу для архівації  | назва файлу для архівації
 
 
+hashsum=$(sha256sum "$archive_name" | cut -d' ' -f1) # отримання хеш-суми архіву."cut" залишає тільки контрольну суму
 # віправка файлу на сервер-отримувач 
-scp "$archive_name" "$receiver_user@$receiver_host:/home/ubuntu/$archive_name" #>> $log_file
-
-# Получение размера файла в битах
-file_size=$(stat -c%s "$archive_name") #поменять на сравнение размера архивов
-
-# Отправка команды на сервер-получатель для проверки файла и ожидание подтверждения
-ssh -T $receiver_user@$receiver_host << EOF | grep -E "File received successfully\. Size match\.|File size doesn't match\. Something went wrong\." >> $log_file
- 
- 
-received_file_size=\$(stat -c%s "/home/ubuntu/$archive_name")
+scp "$archive_name" "$receiver_user@$receiver_host:/home/ubuntu/$archive_name" # якщо потрібно - можна створити змінну для місця зберігання
 
 
+# вычисление SHA256-хеш-суммы полученного файла на удаленном сервере и сравнение со значением отправленной хеш-суммы
 
-  if [ \$received_file_size -eq $file_size ]; then
-    echo "$current_date File received successfully. Size match." # можно добавить имя архива, который был доставлен
-  else
-    echo "$current_date File size doesn't match. Something went wrong."
-  fi
-EOF
+ssh $receiver_user@$receiver_host "sha256sum /home/ubuntu/$archive_name" | awk '{print $1}' | grep "$hashsum" > /dev/null 2>&1 && \
+echo "$current_date File received successfully. Hashsum match." >> $log_file || \
+echo "$current_date Hashsum doesn't match. Something went wrong." >> $log_file
+# на ремоут-хості перевіряється хеш-сума архіву, "awk" вирізає саме значення суми з stdout а grep шукає в виводі "awk" змінну $hashsum, яка
+# яка містить хеш-суму архіву, отриману до відправки на хості-віправнику.якщо результат роботи grep=0,виконується перша echo,якщо не 0-то друга 
+
+
 
 # в конце можно добавить удаление бекапа если условие тру-можно добавить цикл для сервера-отправителя. например:
 #if [ $? -eq 0 ]; then
-#    rm -f $archive_name
+#    rm -f $archive_name + сам файл с бекапом
 #fi
+# Архив создается там, откуда запускается команда
