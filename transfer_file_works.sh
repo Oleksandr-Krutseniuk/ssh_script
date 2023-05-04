537aae876d4b0629cc50850290ec08a0027a60a3
sasha_kr <a.krutseniuk@gmail.com>
2023-05-04 13:08:13 +0300

    hash_sums_check

--- transfer_file_works.sh
+++ transfer_file_works.sh
@@ -15,8 +15,9 @@ while true; do
 # архівація файлу
 archive_name="$(basename ${file_path}).tar.gz" # тут буде назва файлу для архівації + ".tar.gz"
  
-tar -czf "$archive_name" -C "$(dirname ${file_path})" "$(basename ${file_path})" &> /dev/null # скинуть вывод, ато он светится в терминале
+tar -cqzf "$archive_name" -C "$(dirname ${file_path})" "$(basename ${file_path})" > /dev/null 2>&1 # скинуть вывод, ато он светится в терминале
 #        | назва архіву    | місце файлу для архівації  | назва файлу для архівації
+# q - вимикає вивід файлу з архівом при запуску скрипта
 
 
 hashsum=$(sha256sum "$archive_name" | cut -d' ' -f1) # отримання хеш-суми архіву."cut" залишає тільки контрольну суму
