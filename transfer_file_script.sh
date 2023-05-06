96a8bc1b5658889e3ed71899d509d3e9550ceedb
sasha_kr <a.krutseniuk@gmail.com>
2023-05-06 07:01:09 +0300

    jornal output improved

--- transfer_file_script.sh
+++ transfer_file_script.sh
@@ -1,10 +1,6 @@
 #!/bin/bash   
-
-
-
-
-# 
-source /home/ubuntu/var_file.txt # файл зі змінними
+ 
+source /home/*****/var_file.txt # файл зі змінними, які імпортуються
 # key= ключ для ssh-коннекту в файлі зі змінними var_file.txt
 # port_number= номер порту ssh-коннекту в файлі зі змінними var_file.txt
 # backup_host= адреса бекап хосту в файлі зі змінними var_file.txt 
@@ -14,11 +10,11 @@ source /home/ubuntu/var_file.txt # файл зі змінними
 # $dbName =                                    > в файлі зі змінними var_file.txt
 # $dbPassword =                                > в файлі зі змінними var_file.txt
 
-file_path="/home/ubuntu/testfile" # файл для відправки
+file_path="/home/*****/testfile" # файл для відправки
 
 
  
-log_file="/home/ubuntu/logfile.log" # файл з результатом перевірки розміру файлу (відправлений=отриманий)
+log_file="/home/*****/logfile.log" # файл з результатом перевірки розміру файлу (відправлений=отриманий)
 current_date=$(date +"%Y-%m-%d %H:%M:%S" ) # дата перевірки розмірів файлів
 attempts=0 # для повідомлення про 2> невдалі перевірки хеш-сум
 max_attempts=5 # для зупинки скрипта при невдалій перевірці хеш-сум 5 разів
@@ -40,31 +36,34 @@ tar -czf "$archive_name" -C "$(dirname ${file_path})" "$(basename ${file_path})"
 hashsum=$(sha256sum "$archive_name" | cut -d' ' -f1) # отримання хеш-суми архіву."cut" залишає тільки контрольну суму
 # віправка файлу на сервер-отримувач
  
-scp -i "$key" -P "$port_number" "$archive_name" "$backup_user@$backup_host:/home/ubuntu/$archive_name" > /dev/null # якщо потрібно - 
+scp -i "$key" -P "$port_number" "$archive_name" "$backup_user@$backup_host:/home/*****/$archive_name" > /dev/null # якщо потрібно - 
 # можна створити змінну для місця зберігання. після scp у терміналі з'являється назва переданого файлу, що не потрібно для крон-джоби. 
 # тому вивід іде в /dev/null 
 
 # перевірка хеш-сум на ремоут-хості та виведення результату в лог-файл на хості-відправнику
-sleep 2 # возможно команда ниже выполняется раньше, чем файл скопируется
-received_hashsum=$(ssh -i "$key" -p "$port_number" $backup_user@$backup_host "sha256sum /home/ubuntu/$archive_name" | awk '{print $1}')
+ 
+received_hashsum=$(ssh -i "$key" -p "$port_number" $backup_user@$backup_host "sha256sum /home/*****/$archive_name" | awk '{print $1}')
   if [ "$hashsum" = "$received_hashsum" ]; then # якщо хеш-суми однакові
     echo "$current_date File received successfully. Hashsum match." >> $log_file
     rm -f "$file_path" "$archive_name" # видаляє оригінальний файл та архів, якщо хеш-суми співпали
     break # завершує цикл якщо хеш-суми співпали
   else # хеш-сумми не співпали
     attempts=$((attempts+1)) # лічильник невдалих перевірок хеш-сум
-    echo "$current_date Hashsum doesn't match. Something went wrong with $archive_name file." >> $log_file
 
+      if [ "$attempts" -eq 1 ]; then # якщо перевірка хеш-сум провалена перший раз
+        echo "$current_date Hashsum doesn't match. Something went wrong with $archive_name file." >> $log_file
+      fi 
 
       if [ "$attempts" -eq "$max_attempts" ]; then # если 5 проверок хеш-сумм провалены
-      echo "ATTENTION!!! $current_date Hashsums didn't match for $archive_name file after $attempts attempts" >> $log_file
-      exit 1 # выход из скрипта
+        echo "ATTENTION!!! $current_date Hashsums didn't match for $archive_name file after $attempts attempts" >> $log_file
+        exit 1 # вихід зі скрипту якщо перевірка чек-сум провалилася 5 разів 
       fi
-
 
       if [ "$attempts" -ge 2 ]; then # 2 або більше невдалих перевірок хеш-сум
         echo "$current_date Hashsums didn't match for $archive_name file after $attempts attempts" >> $log_file
       fi
+
+
   fi
 
 done
